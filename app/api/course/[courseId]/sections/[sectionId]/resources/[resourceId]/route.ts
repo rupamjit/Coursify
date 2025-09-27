a43d097838340b013198994191dc073c2f13136b
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-09-27 22:55:56 +0530

    delete resource api route

---
+++ app/api/course/[courseId]/sections/[sectionId]/resources/[resourceId]/route.ts
@@ -0,0 +1,48 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/../lib/db";
+
+export const DELETE = async (
+  req: NextRequest,
+  {
+    params,
+  }: { params: { courseId: string; sectionId: string; resourceId: string } }
+) => {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const { courseId, sectionId, resourceId } = params;
+    const course = await db.course.findUnique({
+      where: {
+        id: courseId,
+        instructorId: userId,
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Course Not Found", { status: 404 });
+    }
+    const section = await db.section.findUnique({
+      where: {
+        id: sectionId,
+        courseId,
+      },
+    });
+    if (!section) {
+      return new NextResponse("Section Not Found", { status: 404 });
+    }
+    await db.resource.delete({
+      where: {
+        id: resourceId,
+        sectionId,
+      },
+    });
+
+    return NextResponse.json("Resource deleted", { status: 200 });
+  } catch (error) {
+    console.log("[resourceId_DELETE", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
