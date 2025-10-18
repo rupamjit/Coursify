f0878486bf61f5980fa826e731d6d75f67ad422e
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-10-18 21:00:49 +0530

    list the courses in the instruction page

--- app/(home)/instructor/courses/page.tsx
+++ app/(home)/instructor/courses/page.tsx
@@ -1,19 +1,43 @@
-import { Button } from '../../../../components/ui/button'
-import { Edit } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "../../../../components/ui/button";
+import { Edit } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { DataTable } from "@/../components/customComponent/DataTable";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "../../../../lib/db";
+import { columns } from "../../../../components/customComponent/Columns";
 
-const page = () => {
+const page = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      instructorId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
-    <div className='pt-2'>
+    <div className="pt-2">
       <Button variant={"primary"} asChild>
         <Link href={"/instructor/create-course"}>
-        <Edit/>
-        Create Course
+          <Edit />
+          Create Course
         </Link>
       </Button>
-    </div>
-  )
-}
 
-export default page
+      <div className="mt-5">
+        <DataTable columns={columns} data={courses} />
+      </div>
+    </div>
+  );
+};
+
+export default page;
